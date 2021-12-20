d2e1999b3a200e1d3d91ae9b9ec31200e6646a80
GitHub Action <action@github.com>
2021-12-20 09:00:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/guotailor/mybatisgenerator/MyBundle.kt
+++ src/main/kotlin/com/github/guotailor/mybatisgenerator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.guotailor.mybatisgenerator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/guotailor/mybatisgenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.guotailor.mybatisgenerator.services
+
+import com.github.guotailor.mybatisgenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
